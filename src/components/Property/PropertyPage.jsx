7b1254ab203eecb307220c992520fd7aa22ca668
Darshan-V <mr.darshanv@gmail.com>
2022-12-27 13:27:42 +0530

    add images and the label for the property

--- src/components/Property/PropertyPage.jsx
+++ src/components/Property/PropertyPage.jsx
@@ -1,17 +1,50 @@
 import React, { useState } from "react"
 import store from "../../../../airbnb-data/store.js"
+import { AiFillStar } from "react-icons/ai"
 
 const PropertyPage = () => {
   const [propertyData, setPropertyData] = useState({ store })
   return (
-    <div className="flex">
-      <div className="flex flex-row flex-wrap">
-        <img src={propertyData.store.homeMainPic}></img>
-        <img src={propertyData.store.carouselPic1}></img>
-        <img src={propertyData.store.carouselPic2}></img>
-        <img src={propertyData.store.carouselPic3}></img>
-        <img src={propertyData.store.carouselPic4}></img>
-        <img src={propertyData.store.carouselPic5}></img>
+    <div className="flex flex-col m-auto">
+      <div className="flex flex-col flex-wrap mt-1">
+        <p className="font-sans font-bold text-lg">
+          {propertyData.store.name},{propertyData.store.type},{" "}
+          {propertyData.store.view}
+        </p>
+        <div className="flex flex-wrap justify-items-center mt-1 ">
+          <span>
+            <AiFillStar />
+          </span>
+          <span>{propertyData.store.stars}</span>
+        </div>
+      </div>
+      <div className="flex flex-row justify-center m-0 h-96">
+        <img
+          src={propertyData.store.homeMainPic}
+          className="w-2/4 h-96 m-1"
+        ></img>
+        <div className="flex max-w-lg overflow-x-hidden">
+          <img
+            src={propertyData.store.carouselPic1}
+            className="w-56 h-auto mr-1"
+          ></img>
+          <img
+            src={propertyData.store.carouselPic2}
+            className="w-auto h-auto mr-1"
+          ></img>
+          <img
+            src={propertyData.store.carouselPic5}
+            className="w-auto h-auto mr-1"
+          ></img>
+          <img
+            src={propertyData.store.carouselPic4}
+            className="w-auto h-auto mr-1 "
+          ></img>
+          <img
+            src={propertyData.store.carouselPic3}
+            className="w-auto h-auto mr-1"
+          ></img>
+        </div>
       </div>
     </div>
   )
