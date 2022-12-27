adbee8c85baca9fb6976407473e9a0d3898f9ea3
Darshan-V <mr.darshanv@gmail.com>
2022-12-27 13:28:38 +0530

    add a component for Reserving the property: TODO

---
+++ src/components/Property/Reservation.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+const Reservation = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <div>
+            <div className="data">
+              <div className="bookit"></div>
+              <ul></ul>
+              <div className="calculations"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Reservation
