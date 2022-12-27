df66c0b6e9ef642ba7af7f569fdc13590a3fb0ad
Darshan-V <mr.darshanv@gmail.com>
2022-12-27 11:15:27 +0530

    implement navigate using react-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Places from "./components/Home/Places"
+import PropertyPage from "./components/Property/PropertyPage"
 
 function App() {
   return (
-    <div className="App">
-      <Places />
+    <div className="flex">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Places />} />
+          <Route path="/property" element={<PropertyPage />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
--- src/components/Home/Places.jsx
+++ src/components/Home/Places.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from "react"
 import store from "../../../../airbnb-data/store.js"
 import Slider from "./Slider.jsx"
+import { useNavigate } from "react-router"
 
 const Places = () => {
   const [propertyData, setPropertyData] = useState(store)
+  const navigate = useNavigate()
 
   return (
-    <div className="flex flex-col flex-auto m-5">
+    <div
+      className="flex flex-col flex-auto m-5 hover:cursor-pointer"
+      onClick={() => {
+        navigate("/property")
+      }}
+    >
       <div className="flex">
         <Slider />
       </div>
