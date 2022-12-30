2962873f9d19e4313396a49e6ff1c64f62cb3b6d
Darshan-V <mr.darshanv@gmail.com>
2022-12-30 11:48:26 +0530

    replace property data : get it from backend:

--- src/components/Property/PropertyPage.jsx
+++ src/components/Property/PropertyPage.jsx
@@ -1,48 +1,61 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { AiFillStar } from "react-icons/ai"
 import store from "../../../../airbnb-data/store.js"
 import Reservation from "./Reservation.jsx"
+import { getHotels, getImages } from "../lib/apiClient.js"
 
 const PropertyPage = () => {
-  const [propertyData, setPropertyData] = useState({ store })
+  const [propertyData, setPropertyData] = useState([])
+  const [hotelImages, setHotelImages] = useState([])
+
+  useEffect(() => {
+    const loadHotelList = async () => {
+      const hotelList = await getHotels()
+      setPropertyData(hotelList)
+      const loadImageList = await getImages(1)
+      setHotelImages(loadImageList)
+    }
+    loadHotelList()
+  }, [])
+
   return (
     <div className="flex flex-col m-auto">
       <div className="flex flex-col flex-wrap mt-1">
         <p className="font-sans font-bold text-lg">
-          {propertyData.store.name},{propertyData.store.type},{" "}
-          {propertyData.store.view}
+          {propertyData[0]?.placename},{propertyData[0]?.type},{" "}
+          {propertyData[0]?.environment}
         </p>
         <div className="flex flex-wrap justify-items-center mt-1 ">
           <span>
             <AiFillStar />
           </span>
-          <span>{propertyData.store.stars}</span>
+          <span>{propertyData[0]?.stars}</span>
         </div>
       </div>
       <div className="flex flex-row justify-center m-0 h-96">
         <img
-          src={propertyData.store.homeMainPic}
+          src={hotelImages[0]?.homemainpicurl}
           className="w-2/4 h-96 m-1"
         ></img>
         <div className="flex max-w-lg overflow-x-hidden">
           <img
-            src={propertyData.store.carouselPic1}
+            src={hotelImages[0]?.carouselpic1url}
             className="w-72 h-auto mr-1"
           ></img>
           <img
-            src={propertyData.store.carouselPic2}
+            src={hotelImages[0]?.carouselpic2url}
             className="w-60 h-auto mr-1"
           ></img>
           <img
-            src={propertyData.store.carouselPic5}
+            src={hotelImages[0]?.carouselpic3url}
             className="w-auto h-auto mr-1"
           ></img>
           <img
-            src={propertyData.store.carouselPic4}
+            src={hotelImages[0]?.carouselpic4url}
             className="w-auto h-auto mr-1 "
           ></img>
           <img
-            src={propertyData.store.carouselPic3}
+            src={hotelImages[0]?.carouselpic5url}
             className="w-auto h-auto mr-1"
           ></img>
         </div>
