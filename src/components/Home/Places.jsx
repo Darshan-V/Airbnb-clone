9234c900c50cbe8d890434cc859f735899e4b2f7
Darshan-V <mr.darshanv@gmail.com>
2023-01-04 17:18:14 +0530

    add route parameters

--- src/components/Home/Places.jsx
+++ src/components/Home/Places.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react"
 import Slider from "./Slider.jsx"
-import { useNavigate } from "react-router"
+import { useNavigate, useParams } from "react-router"
 import { getHotels } from "../lib/apiClient.js"
+import Topbar from "../Topbar/Topbar.jsx"
 
 const Places = () => {
   const [propertyData, setPropertyData] = useState([])
@@ -16,26 +17,30 @@ const Places = () => {
   }, [propertyData.id])
 
   const navigate = useNavigate()
+  const params = useParams()
 
   return (
-    <div className="flex flex-row m-auto pt-20">
+    <div className="flex flex-wrap m-auto w-full">
+      <Topbar />
       {propertyData.map((property, i) => (
-        <div className="flex flex-col flex-auto  m-5 " key={i}>
-          <div className="flex">
-            <Slider id={property.id} />
-          </div>
+        <div className="flex flex-row" key={i}>
+          <div className="flex flex-col flex-auto  m-5 " key={i}>
+            <div className="flex">
+              <Slider id={property.id} />
+            </div>
 
-          <div
-            className="flex flex-row hover:cursor-pointer"
-            onClick={() => {
-              navigate(`/property/${property.id}`)
-            }}
-          >
-            <p className="font-thin text-gray-800 w-60 h-12 overflow-hidden">
-              {property?.name}, {property?.address?.location}
-            </p>
+            <div
+              className="flex flex-row hover:cursor-pointer"
+              onClick={() => {
+                navigate(`/property/${property.id}/${params.userid}`)
+              }}
+            >
+              <p className="font-thin text-gray-800 w-60 h-12 overflow-hidden">
+                {property?.name}, {property?.address?.location}
+              </p>
+            </div>
+            <p className="label places-price">Rs {property?.price} night</p>
           </div>
-          <p className="label places-price">Rs {property?.price} night</p>
         </div>
       ))}
     </div>
