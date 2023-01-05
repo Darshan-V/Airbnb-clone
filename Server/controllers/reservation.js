dc3347052eeaef100c7e7750ce508db12d2b53c4
Darshan-V <mr.darshanv@gmail.com>
2023-01-05 16:01:14 +0530

    remove console.log

--- Server/controllers/reservation.js
+++ Server/controllers/reservation.js
@@ -2,12 +2,6 @@ import { pool } from "../models/config/init.js"
 
 const checkCheckIn = async (checkIn, hotelId) => {
   try {
-    console.log(
-      "🚀 ~ file: reservation.js:4 ~ checkCheckIn ~ checkIn",
-      checkIn,
-      hotelId
-    )
-
     const isReserved = await pool.query(
       `select exists (select true from bookings where check_in = $1 and property_id = $2)`,
       [checkIn, hotelId]
