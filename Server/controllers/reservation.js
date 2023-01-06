8eeb1593a0b5f9061a2e86ff774c691d25b49231
Darshan-V <mr.darshanv@gmail.com>
2023-01-06 12:44:08 +0530

    get date between checkin and checkout for a property

--- Server/controllers/reservation.js
+++ Server/controllers/reservation.js
@@ -12,4 +12,40 @@ const checkCheckIn = async (checkIn, hotelId) => {
   }
 }
 
-export { checkCheckIn }
+const getDaysArray = function (start, end) {
+  const arr = []
+  for (
+    const dt = new Date(start);
+    dt <= new Date(end);
+    dt.setDate(dt.getDate() + 1)
+  ) {
+    arr.push(new Date(dt))
+  }
+  return arr
+}
+
+const getDatesInbetween = async (hotelId) => {
+  const start = await pool.query(
+    `select check_in from bookings where property_id = $1`,
+    [hotelId]
+  )
+  const end = await pool.query(
+    `select check_out from bookings where property_id = $1`,
+    [hotelId]
+  )
+  const startDate = start.rows
+  const endDate = end.rows
+
+  const daylist = getDaysArray(
+    new Date(startDate[0]?.check_in),
+    new Date(endDate[0]?.check_out)
+  )
+  const dateList = daylist
+    .map((date) => date.toISOString().slice(0, 10))
+    .join(", ")
+  return JSON.stringify(dateList)
+}
+
+//TODO check betweeen checkin and checkout
+
+export { checkCheckIn, getDatesInbetween }
