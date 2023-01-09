d1945daf381e731abb6aa0b289980f1826bb84d4
Darshan-V <mr.darshanv@gmail.com>
2023-01-09 17:33:53 +0530

    add try catch for, add check available slot api

--- Server/app.js
+++ Server/app.js
@@ -4,7 +4,7 @@ import { initDB } from "./models/config/init.js"
 import { getHotelById, getHotels, getImages } from "./models/hotelModel.js"
 import { reserveSlot } from "./models/bookingsModel.js"
 import { getUserById, getUserByUserName } from "./models/userModel.js"
-import { checkCheckIn, getDatesInbetween } from "./controllers/reservation.js"
+import { checkAvailableSlots } from "./controllers/reservation.js"
 
 const PORT = 8000
 
@@ -14,67 +14,77 @@ app.use(cors())
 initDB()
 
 app.get("/hotels", async (req, res) => {
-  const hotels = await getHotels()
-  res.json(hotels)
+  try {
+    const hotels = await getHotels()
+    res.json(hotels)
+  } catch (err) {
+    res.sendStatus(500)
+  }
 })
 
 app.get("/hotel/:id/images", async (req, res) => {
-  const hotelId = req.params.id
-  const images = await getImages(hotelId)
-  console.log(images[0].imageUrl)
-  res.json(images[0].imageurl)
+  try {
+    const hotelId = req.params.id
+    const images = await getImages(hotelId)
+    console.log(images[0].imageUrl)
+    res.json(images[0].imageurl)
+  } catch (err) {
+    res.sendStatus(500)
+  }
 })
 
 app.get("/hotel/:id", async (req, res) => {
-  const hotelId = req.params.id
-  const hotel = await getHotelById(hotelId)
-  res.json(hotel[0])
+  try {
+    const hotelId = req.params.id
+    const hotel = await getHotelById(hotelId)
+    res.json(hotel[0])
+  } catch (err) {
+    res.sendStatus(500)
+  }
 })
 
 app.get("/user/:id", async (req, res) => {
-  const userId = req.params.id
-  if (Number(userId)) {
-    const user = await getUserById(userId)
-    res.json(user)
-  } else {
-    const user = await getUserByUserName(userId)
-    res.json(user)
-  }
-})
-
-// app.get("/validate/:checkin?/:id", async (req, res) => {
-//   const checkIn = req.params.checkin
-//   const hotelId = req.params.id
-
-//   const checkStartDate = await checkCheckIn(checkIn, hotelId)
-//   res.json(checkStartDate)
-// })
-
-app.get("/booking/getDates/:propertyId", async (req, res) => {
   try {
-    const hotelId = req.params.propertyId
-    const getDates = await getDatesInbetween(hotelId)
-    res.json(getDates)
+    const userId = req.params.id
+    if (Number(userId)) {
+      const user = await getUserById(userId)
+      res.json(user)
+    } else {
+      const user = await getUserByUserName(userId)
+      res.json(user)
+    }
   } catch (err) {
-    res.json(err)
+    res.sendStatus(500)
   }
 })
-//FIXME route params /hotel/:id/...
-app.post("/hotel/:id/booking/", async (req, res) => {
-  const hotelId = req.params.id
-  const { checkIn, checkOut, userId, total } = req.body
-  console.log("🚀 ~ file: app.js:65 ~ app.post ~ req.body", req.body)
 
-  // TODO error handling
-  //FIXME total-> backend, userId->from the session(login)
-  const confirmBooking = await reserveSlot(
-    checkIn,
-    checkOut,
-    hotelId,
-    userId,
-    total
-  )
-  res.json(req.body)
+app.get("/check/slots/:hotelId/:checkIn/:checkOut", async (req, res) => {
+  try {
+    const { hotelId, checkIn, checkOut } = req.params
+    const isAvailable = await checkAvailableSlots(checkIn, checkOut, hotelId)
+    res.json(isAvailable)
+  } catch (err) {
+    res.sendStatus(500)
+  }
+})
+
+app.post("/hotel/:id/booking/", async (req, res) => {
+  try {
+    const hotelId = req.params.id
+    const { checkIn, checkOut, userId, total } = req.body
+    console.log("🚀 ~ file: app.js:65 ~ app.post ~ req.body", req.body)
+
+    const confirmBooking = await reserveSlot(
+      checkIn,
+      checkOut,
+      hotelId,
+      userId,
+      total
+    )
+    res.json(confirmBooking)
+  } catch (err) {
+    res.sendStatus(500)
+  }
 })
 
 app.listen(PORT, () => {
