84b166e4454b452f27ba53be9481eba5324d9860
Darshan-V <mr.darshanv@gmail.com>
2023-01-16 11:22:13 +0530

    create controller for hotel route

---
+++ Server/controllers/hotels.js
@@ -0,0 +1,33 @@
+import { getHotels, getImages, getHotelById } from "../models/hotelModel.js"
+
+async function getAllHotels(req, res) {
+  try {
+    const hotels = await getHotels()
+    res.json(hotels)
+  } catch (err) {
+    res.sendStatus(500)
+  }
+}
+
+async function getHotelImages(req, res) {
+  try {
+    const hotelId = req.params.id
+    const images = await getImages(hotelId)
+    console.log(images[0].imageUrl)
+    res.json(images[0].imageurl)
+  } catch (err) {
+    res.sendStatus(500)
+  }
+}
+
+async function getHotelByHotelId(req, res) {
+  try {
+    const hotelId = req.params.id
+    const hotel = await getHotelById(hotelId)
+    res.json(hotel[0])
+  } catch (err) {
+    res.sendStatus(500)
+  }
+}
+
+export { getAllHotels, getHotelImages, getHotelByHotelId }
