5e3277c814bf32a0e5342764aa348e43e587fb42
Darshan-V <mr.darshanv@gmail.com>
2023-01-16 11:43:29 +0530

    move all routes to the routes folder

--- Server/app.js
+++ Server/app.js
@@ -2,13 +2,11 @@ import express from "express"
 import cors from "cors"
 import session from "express-session"
 import { initDB } from "./models/config/init.js"
-import { getHotelById, getHotels, getImages } from "./models/hotelModel.js"
 import { reserveSlot } from "./models/bookingsModel.js"
-import { checkAvailableSlots } from "./controllers/reservation.js"
 import { routes as loginRouter } from "./routes/Login.js"
 import { routes as hotelRouter } from "./routes/hotel.js"
 import { routes as userRouter } from "./routes/user.js"
-import { authSession } from "./controllers/authMiddleware.js"
+import { routes as reservationRouter } from "./routes/reservation.js"
 
 const PORT = 8000
 
@@ -32,38 +30,7 @@ app.use("/", hotelRouter)
 
 app.use("/", userRouter)
 
-app.get(
-  "/check/slots/:hotelId/:checkIn/:checkOut",
-  authSession,
-  async (req, res) => {
-    try {
-      const { hotelId, checkIn, checkOut } = req.params
-      const bookedSlots = await checkAvailableSlots(checkIn, checkOut, hotelId)
-      res.json(bookedSlots)
-    } catch (err) {
-      res.sendStatus(500)
-    }
-  }
-)
-
-app.post("/hotel/:id/booking/", authSession, async (req, res) => {
-  try {
-    const hotelId = req.params.id
-    const { checkIn, checkOut, userId, total } = req.body
-    console.log("🚀 ~ file: app.js:65 ~ app.post ~ req.body", req.body)
-
-    const confirmBooking = await reserveSlot(
-      checkIn,
-      checkOut,
-      hotelId,
-      userId,
-      total
-    )
-    res.json(confirmBooking)
-  } catch (err) {
-    res.sendStatus(500)
-  }
-})
+app.use("/", reservationRouter)
 
 //-------------------------------test oauth---------------------------------------------
 
