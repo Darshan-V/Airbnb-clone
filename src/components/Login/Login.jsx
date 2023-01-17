a58659f4a438747bcae6f97b311822bfa4f92efe
Darshan-V <mr.darshanv@gmail.com>
2023-01-17 11:04:27 +0530

    change the login page layout

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,55 +1,48 @@
 import React, { useState, useEffect } from "react"
-import { checkUser } from "./utils/config.js"
-import { useNavigate } from "react-router"
-import googleButton from "./btn_google_dark_pressed_ios.svg"
-import { login } from "./../lib/apiClient.js"
 
 const Login = () => {
   const [userName, setUserName] = useState("")
-  const navigate = useNavigate()
-
-  const validateUser = async (uName) => {
-    if (await checkUser(uName)) {
-      const userid = await checkUser(uName)
-      navigate(`/home/${userid}`)
-    }
-  }
-  const googleLogin = async () => {
-    await login()
-  }
+  const [password, setPassword] = useState("")
 
   return (
-    <div className=" flex flex-col justify-center m-auto  flex-auto w-92">
-      {/* <label>Username:</label>
+    <div className=" flex flex-col justify-center m-auto  flex-auto w-92 border border-black p-3">
+      <p className="text font-semibold text-2xl pb-3">Login</p>
+      <label>Username</label>
       <input
         placeholder="Username"
-        className="w-80 border rounded-sm "
+        className="w-80 border rounded-sm h-10 border-black"
         onChange={(e) => {
           setUserName(e.target.value)
         }}
       />
+
+      {userName.length === 0 ? (
+        <p className="text text-red-600">* Email Required</p>
+      ) : null}
+
+      <label>Password</label>
+      <input
+        placeholder="password"
+        type="password"
+        className="w-80 border rounded-sm h-10 border-black"
+        onChange={(e) => {
+          setPassword(e.target.value)
+        }}
+      />
+
+      {password.length === 0 ? (
+        <p className="text text-red-600">* Password Required</p>
+      ) : null}
+
       <div className="ml-auto p-2 w-40">
-        <button
-          className="border p-2 w-full rounded-md font-bold"
-          onClick={() => {
-            validateUser(userName)
-          }}
-        >
+        <button className="border p-2 w-full rounded-md font-bold">
           Login
         </button>
-      </div> */}
+      </div>
+      <br />
+      <p className="m-auto">-or-</p>
       <div className="flex flex-row justify-center w-full p-1 m-auto bg-blue-600 border rounded-md">
-        <div className="m-auto">
-          <p className="text text-white">Signin with Google</p>
-        </div>
-        <div
-          className="ml-auto"
-          onClick={() => {
-            googleLogin()
-          }}
-        >
-          <img src={googleButton} className="bg-blue-600 w-14 h-14" />
-        </div>
+        <button>Signup</button>
       </div>
     </div>
   )
