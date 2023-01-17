3e105fc9434045a5074bdff936e4c87959d0e7c3
Darshan-V <mr.darshanv@gmail.com>
2023-01-17 15:49:39 +0530

    fix cors error add origin

--- Server/app.js
+++ Server/app.js
@@ -12,7 +12,9 @@ const PORT = 8000
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(
+  cors({ origin: "http://localhost:5173", methods: "GET,POST,PUT,DELETE" })
+)
 initDB()
 
 app.use("/", signupRouter)
