bf4b885a46d5ff066bdb32ad1a4078a8d4a282a5
Darshan-V <mr.darshanv@gmail.com>
2023-01-17 10:50:39 +0530

    add controller to handle signup

---
+++ Server/controllers/Signup.js
@@ -0,0 +1,29 @@
+import { registerNewUser } from "./user.js"
+
+async function signup(req, res) {
+  try {
+    let { name, email, password } = req.body
+    name = name.trim()
+    email = email.trim()
+    password = password.trim()
+    if (name.length === 0 || email.length === 0 || password.length === 0) {
+      res.status(406).json("invalid input details")
+    } else if (!/^[a-zA-Z]*$/.test(name)) {
+      res.status(406).json("invalid username")
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      res.status(406).json("invalid email address")
+    } else if (password.length < 8) {
+      res
+        .status(406)
+        .json("password length should be greater than 8 characters")
+    } else {
+      const newUser = await registerNewUser(name, email, password)
+      res.json(`${name} registered proceed to login`)
+    }
+  } catch (err) {
+    console.log(err.stack)
+    res.sendStatus(500)
+  }
+}
+
+export { signup }
