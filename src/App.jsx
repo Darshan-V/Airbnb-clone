0168a3a7c819662e416d823d1ead72bb52140d29
Darshan-V <mr.darshanv@gmail.com>
2023-01-17 15:54:01 +0530

    add signup react-route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Places from "./components/Home/Places"
 import Login from "./components/Login/Login"
+import Signup from "./components/Login/Signup"
 import BookingSlots from "./components/Property/BookingSlots"
 import PropertyImages from "./components/Property/PropertyImages"
 import PropertyPage from "./components/Property/PropertyPage"
@@ -13,7 +14,7 @@ function App() {
         <Topbar />
         <Routes>
           <Route path="/" element={<Login />} />
-
+          <Route path="signup" element={<Signup />} />
           <Route path="/home/:userid?" element={<Places />} />
           <Route path="/property/:id?/:userid?" element={<PropertyPage />} />
           <Route
