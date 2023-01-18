9d053518e24fd7c136ca079cdf9229fa8bbe58a2
Darshan-V <mr.darshanv@gmail.com>
2023-01-18 10:17:24 +0530

    implement verify hashed data with entered data for user signin

--- Server/controllers/authenticateUser.js
+++ Server/controllers/authenticateUser.js
@@ -1,8 +1,20 @@
 import { getUserByEmail } from "../models/userModel.js"
+import { verifyHashedPassword } from "./utils.js"
+
 async function authenticateUser(email, password) {
   try {
     const fetchedUser = await getUserByEmail(email)
-    return fetchedUser
+    if (!fetchedUser.length) {
+      return "user not exist please signup"
+    } else {
+      const hashedPassword = fetchedUser[0].password
+      const matchPassword = await verifyHashedPassword(password, hashedPassword)
+      if (!matchPassword) {
+        return "invalid password"
+      } else {
+        return { name: fetchedUser[0].name, email: fetchedUser[0].email }
+      }
+    }
   } catch (error) {
     console.log(error)
   }
