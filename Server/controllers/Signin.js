c75580e95c0b38c781bc7ffe8c36773ef6a84869
Darshan-V <mr.darshanv@gmail.com>
2023-01-18 10:18:36 +0530

    catch error from the authenticateUser function and send it as response

--- Server/controllers/Signin.js
+++ Server/controllers/Signin.js
@@ -3,16 +3,15 @@ import { authenticateUser } from "./authenticateUser.js"
 async function signin(req, res) {
   try {
     const { email, password } = req.body
-    email = email.trim()
-    password = password.trim()
+    console.log(email, password)
     if (email.length === 0 || password.length === 0) {
       res.status(406).json("empty credentials")
     } else {
       const authenticatedUser = await authenticateUser(email, password)
-      res.status(200).json("valid user")
+      res.status(200).json(authenticatedUser)
     }
-  } catch (err) {
-    res.json(err)
+  } catch (error) {
+    res.json(error)
   }
 }
 
