98b1d384e414506cb241f392a89bb452f27e72b4
Darshan-V <mr.darshanv@gmail.com>
2023-01-19 09:57:17 +0530

    test:generate jwt on login

--- Server/controllers/authenticateUser.js
+++ Server/controllers/authenticateUser.js
@@ -1,5 +1,8 @@
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
 import { getUserByEmail } from "../models/userModel.js"
 import { verifyHashedPassword } from "./utils.js"
+dotenv.config()
 
 async function authenticateUser(email, password) {
   try {
@@ -12,7 +15,15 @@ async function authenticateUser(email, password) {
       if (!matchPassword) {
         return "invalid password"
       } else {
-        return { name: fetchedUser[0].name, email: fetchedUser[0].email }
+        console.log(process.env.JWT_SECRET)
+        const token = jwt.sign(
+          { email: fetchedUser[0].email, name: fetchedUser[0].name },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: "1h"
+          }
+        )
+        return token
       }
     }
   } catch (error) {
