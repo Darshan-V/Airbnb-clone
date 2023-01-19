a661e312396f58cef39a94f8a988c4695ff24c38
Darshan-V <mr.darshanv@gmail.com>
2023-01-19 15:25:49 +0530

    set cors credentials

--- Server/app.js
+++ Server/app.js
@@ -12,7 +12,11 @@ const PORT = 8000
 const app = express()
 app.use(express.json())
 app.use(
-  cors({ origin: "http://localhost:5173", methods: "GET,POST,PUT,DELETE" })
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true
+  })
 )
 initDB()
 
