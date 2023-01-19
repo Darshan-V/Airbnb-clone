3bd0e9776de41dedca12145657758ba4b81a226b
Darshan-V <mr.darshanv@gmail.com>
2023-01-19 15:22:00 +0530

    create middleware to verify jwt from the client

--- Server/controllers/authMiddleware.js
+++ Server/controllers/authMiddleware.js
@@ -1,22 +1,22 @@
-import { pool } from "../models/config/init.js"
-async function authSession(req, res, next) {
-  const sessionData = req.session
-  //   console.log(
-  //     "🚀 ~ file: authMiddleware.js:3 ~ authSession ~ sessionData",
-  //     sessionData
-  //   )
-  pool.query(
-    "select * from users where email = $1",
-    [sessionData.username],
-    function (err, rows) {
-      if (err) throw err
-      if (rows.length) {
-        const user = rows[0]
-        req.session.username = user.email
-      }
-    }
-  )
-  next()
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+dotenv.config()
+
+const verifyUserToken = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).json("unauthorized request")
+  }
+  const token = req.headers["authorization"].split("")[1]
+  if (!token) {
+    return res.status(401).json("access denied no token found")
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = decoded.user
+    next()
+  } catch (error) {
+    res.status(400).json("invalid token")
+  }
 }
 
-export { authSession }
+export { verifyUserToken }
