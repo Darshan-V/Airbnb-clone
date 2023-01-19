dd596ec39ddefc4ca155b60418d5ad09afd65b43
Darshan-V <mr.darshanv@gmail.com>
2023-01-19 15:25:04 +0530

    fix route names to plural

--- Server/routes/reservation.js
+++ Server/routes/reservation.js
@@ -1,9 +1,14 @@
 import express from "express"
 import { checkAvailablity, makeBooking } from "../controllers/reservation.js"
+import { verifyToken } from "../controllers/verifyToken.js"
 const routes = express.Router()
 
-routes.get("/check/slots/:hotelId/:checkIn/:checkOut", checkAvailablity)
+routes.get(
+  "/check/slots/:hotelId/:checkIn/:checkOut",
+  verifyToken,
+  checkAvailablity
+)
 
-routes.post("/hotel/:id/booking/", makeBooking)
+routes.post("/hotel/:id/booking/", verifyToken, makeBooking)
 
 export { routes }
