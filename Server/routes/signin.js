0732211087c3e3026f80173e9d5becae2c917e18
Darshan-V <mr.darshanv@gmail.com>
2023-01-20 12:07:43 +0530

    add verify token middleware to the login if the cookie exist and login automatcally

--- Server/routes/signin.js
+++ Server/routes/signin.js
@@ -1,7 +1,8 @@
 import express from "express"
 import { signin } from "../controllers/Signin.js"
+import { verifyToken } from "../controllers/verifyToken.js"
 const routes = express.Router()
 
-routes.post("/signin", signin)
+routes.post("/signin", verifyToken, signin)
 
 export { routes }
