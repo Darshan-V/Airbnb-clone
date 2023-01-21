8cc0025d8be30638056ab249fe8c70eabe072e41
Darshan-V <mr.darshanv@gmail.com>
2023-01-21 22:40:26 +0530

    remove middleware for login

--- Server/routes/signin.js
+++ Server/routes/signin.js
@@ -1,8 +1,7 @@
 import express from "express"
 import { signin } from "../controllers/Signin.js"
-import { verifyToken } from "../controllers/verifyToken.js"
 const routes = express.Router()
 
-routes.post("/signin", verifyToken, signin)
+routes.post("/signin", signin)
 
 export { routes }
