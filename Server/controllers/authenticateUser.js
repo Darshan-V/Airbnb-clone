35cd0013dc1693d2866516b51428ad5590505333
Darshan-V <mr.darshanv@gmail.com>
2023-01-21 22:35:47 +0530

    fix trycatch in util function, fix return error message to throw new error instance

--- Server/controllers/authenticateUser.js
+++ Server/controllers/authenticateUser.js
@@ -5,33 +5,28 @@ import { verifyHashedPassword } from "./utils.js"
 dotenv.config()
 
 async function authenticateUser(email, password) {
-  try {
-    const fetchedUser = await getUserByEmail(email)
-    if (!fetchedUser.length) {
-      return "user not exist please signup"
-    } else {
-      const hashedPassword = fetchedUser[0].password
-      const matchPassword = await verifyHashedPassword(password, hashedPassword)
-      if (!matchPassword) {
-        return "invalid password"
-      } else {
-        const token = jwt.sign(
-          {
-            email: fetchedUser[0].email,
-            id: fetchedUser[0].id,
-            name: fetchedUser[0].name
-          },
-          process.env.JWT_SECRET,
-          {
-            expiresIn: "1h"
-          }
-        )
-        return token
-      }
-    }
-  } catch (error) {
-    console.log(error)
+  const fetchedUser = await getUserByEmail(email)
+  if (!fetchedUser.length) {
+    throw new Error("user not exist please signup")
   }
+
+  const hashedPassword = fetchedUser[0].password
+  const matchPassword = await verifyHashedPassword(password, hashedPassword)
+  if (matchPassword === false) {
+    throw new Error("invalid password")
+  }
+  const token = jwt.sign(
+    {
+      email: fetchedUser[0].email,
+      id: fetchedUser[0].id,
+      name: fetchedUser[0].name
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1h"
+    }
+  )
+  return token
 }
 
 export { authenticateUser }
