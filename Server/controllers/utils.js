8faa84b8c6172a7bb0bdbd886717cb9b5dc6fe91
Darshan-V <mr.darshanv@gmail.com>
2023-01-21 22:38:09 +0530

    fix remove trycatch for util function

--- Server/controllers/utils.js
+++ Server/controllers/utils.js
@@ -1,12 +1,8 @@
 import bcrypt from "bcrypt"
 
 const verifyHashedPassword = async (password, hashedPassword) => {
-  try {
-    const match = await bcrypt.compare(password, hashedPassword)
-    return match
-  } catch (error) {
-    return error
-  }
+  const match = await bcrypt.compare(password, hashedPassword)
+  return match
 }
 
 export { verifyHashedPassword }
