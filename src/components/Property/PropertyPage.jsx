35d0e22702c34366cfdc004c05382eb074b7651c
Darshan-V <mr.darshanv@gmail.com>
2023-01-30 15:42:49 +0530

    refactor property page add images

--- src/components/Property/PropertyPage.jsx
+++ src/components/Property/PropertyPage.jsx
@@ -4,18 +4,21 @@ import { getImages, getHotelById } from "../lib/apiClient.js"
 import { useNavigate, useParams } from "react-router"
 
 const PropertyPage = () => {
-  const [propertyData, setPropertyData] = useState([])
-  const [hotelImages, setHotelImages] = useState([])
+  const [propertyData, setPropertyData] = useState({
+    property: "",
+    loading: true
+  })
+  const [hotelImages, setHotelImages] = useState({ images: [], loading: true })
   const params = useParams()
   const hotelId = params.id
 
   const loadHotelList = async () => {
     const hotelList = await getHotelById(hotelId)
     if (hotelList === "unauthorized") navigate("/")
-    setPropertyData(hotelList)
+    setPropertyData({ property: hotelList, loading: false })
     const loadImageList = await getImages(hotelId)
     const images = loadImageList
-    setHotelImages(images)
+    setHotelImages({ images: images, loading: false })
   }
 
   useEffect(() => {
@@ -23,29 +26,35 @@ const PropertyPage = () => {
   }, [propertyData.id])
 
   const navigate = useNavigate()
-
+  console.log(hotelImages)
   return (
-    <div className="flex flex-col m-auto">
-      <div className="flex flex-col  m-3">
+    <div className="flex flex-col w-full m-auto">
+      <div className="flex flex-col w-4/6 m-auto">
         <p className="font-sans font-bold text-lg">
-          {propertyData?.name}, {propertyData?.type}
+          {propertyData?.property.name}, {propertyData?.property.type}
         </p>
-        <div className=" flex justify-center w-1/2 ">
-          <img src={hotelImages[0]} />
+        <div className="flex flex-col w-full flex-wrap h-96 overflow-hidden no-scrollbar ">
+          {hotelImages.images.map((image, i) => (
+            <div className="m-1 h-96" key={i}>
+              <div>
+                <img src={image} className="h-96 w-96" />
+              </div>
+            </div>
+          ))}
         </div>
-        <div className="flex justify-start m-1 ">
+        <div className="flex justify-start ml-auto ">
           <button
-            className="border border-black"
+            className=" border-2 border-indigo-200 border-b-indigo-500 rounded-md"
             onClick={() => {
               navigate(`/property_images/${hotelId}`)
             }}
           >
-            Show images
+            Show more images
           </button>
         </div>
       </div>
-      <div className="flex justify-end sticky">
-        <Reservation />
+      <div className="flex justify-end sticky top-0">
+        <Reservation price={propertyData?.property?.price} />
       </div>
     </div>
   )
