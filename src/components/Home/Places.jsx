3e4ef23cd4d1352070c4fd4d2580a310923ded15
Darshan-V <mr.darshanv@gmail.com>
2023-01-30 15:41:49 +0530

    fix layout

--- src/components/Home/Places.jsx
+++ src/components/Home/Places.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react"
 import { useNavigate } from "react-router"
 import { getHotels } from "../lib/apiClient.js"
 import HomeTab from "./HomeTab.jsx"
+import Topbar from "./../Topbar/Topbar"
 
 const Places = () => {
   const [propertyData, setPropertyData] = useState([])
@@ -27,35 +28,38 @@ const Places = () => {
   // }
 
   return (
-    <div className="flex flex-wrap justify-center m-auto w-full">
-      <HomeTab />
-      {propertyData.map((property, i) => (
-        <div className="flex flex-row" key={i}>
-          <div className="flex flex-col flex-auto m-2 " key={i}>
-            <div className=" w-80 h-60 border rounded-md ">
-              <img
-                src={propertyData[i]?.imageurl[i]}
-                className="border rounded-lg w-80 h-60"
-              />
-            </div>
+    <div className="flex flex-wrap justify-center w-full">
+      <Topbar />
+      <div className="flex w-full flex-wrap justify-center m-auto">
+        <HomeTab />
+        {propertyData.map((property, i) => (
+          <div className="flex flex-row" key={i}>
+            <div className="flex flex-col flex-auto m-2 " key={i}>
+              <div className=" w-80 h-60 border rounded-md ">
+                <img
+                  src={propertyData[i]?.imageurl[i]}
+                  className="border rounded-lg w-80 h-60"
+                />
+              </div>
 
-            <div
-              className="flex flex-col hover:cursor-pointer"
-              onClick={() => {
-                navigate(`/property/${property.id}`)
-              }}
-            >
-              <p className="font-bold text-gray-800 w-60 h-6 overflow-hidden">
-                {property?.name}
-              </p>
-              <p className="font-thin text-gray-700 w-60 h-6 overflow-hidden">
-                {property?.address?.location}
-              </p>
+              <div
+                className="flex flex-col hover:cursor-pointer"
+                onClick={() => {
+                  navigate(`/property/${property.id}`)
+                }}
+              >
+                <p className="font-bold text-gray-800 w-60 h-6 overflow-hidden">
+                  {property?.name}
+                </p>
+                <p className="font-thin text-gray-700 w-60 h-6 overflow-hidden">
+                  {property?.address?.location}
+                </p>
+              </div>
+              <p className="label places-price">Rs {property?.price} night</p>
             </div>
-            <p className="label places-price">Rs {property?.price} night</p>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   )
 }
