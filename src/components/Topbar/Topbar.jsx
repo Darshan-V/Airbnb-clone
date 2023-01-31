cb89a65852faf752ac48dd7a1f389e370eb755d1
Darshan-V <mr.darshanv@gmail.com>
2023-01-31 16:22:26 +0530

    add search bar:input

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react"
 import { TbAerialLift, TbSearch } from "react-icons/tb"
-import { useNavigate } from "react-router"
+import { useNavigate, useParams } from "react-router"
 
 const Topbar = () => {
   const [search, setSearch] = useState(false)
   const navigate = useNavigate()
   return (
-    <div className="flex justify-between w-full m-auto p-5 bg-white-300  sticky top-0 bg-white">
+    <div className="flex justify-between w-full m-auto bg-white-300  sticky top-0 bg-white">
       <div
         className="flex justify-center mr-auto rounded-lg "
         onClick={() => {
@@ -15,6 +15,7 @@ const Topbar = () => {
       >
         <TbAerialLift className="text text-6xl font text-yellow-600 hover:text-yellow-400" />
       </div>
+
       <div className="flex ml-auto">
         <div className="flex flex-row">
           <TbSearch className="m-auto text-yellow-700 text-3xl font-bold" />
