f555b01bd24e09a5ca8795fe9c06109837efb019
Darshan-V <mr.darshanv@gmail.com>
2023-01-31 16:23:23 +0530

    add image carousel to property page

---
+++ src/components/Property/ImageCarousel.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import { Carousel } from "react-responsive-carousel"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+
+const ImageCarousel = ({ images }) => {
+  return (
+    <div className="w-3/4 mr-auto rounded-lg">
+      <Carousel
+        showArrows={true}
+        autoPlay={false}
+        width="100%"
+        dynamicHeight={false}
+        className="m-auto "
+      >
+        {images.map((image, i) => (
+          <div key={i}>
+            <img src={image} className="rounded-lg " />
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  )
+}
+
+export default ImageCarousel
