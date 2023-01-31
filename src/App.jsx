56e691c426120fb5ae6cc2ed5add787d0d9f2e35
Darshan-V <mr.darshanv@gmail.com>
2023-01-31 16:19:06 +0530

    add topbar to the app

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import Signup from "./components/Login/Signup"
 import BookingSlots from "./components/Property/BookingSlots"
 import PropertyImages from "./components/Property/PropertyImages"
 import PropertyPage from "./components/Property/PropertyPage"
+import Topbar from "./components/Topbar/Topbar"
 
 function App() {
   return (
     <div className="flex flex-col m-12">
       <Router>
+        <Topbar />
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="signup" element={<Signup />} />
-          <Route path="/home?" element={<Places />} />
+          <Route path="/home/:type?" element={<Places />} />
           <Route path="/property/:id?/?" element={<PropertyPage />} />
           <Route path="/booking/:propertyname?" element={<BookingSlots />} />
           <Route path="/property_images/:id" element={<PropertyImages />} />
