a65d6644eece62264b9306b35a2afc57fb09b76d
Darshan-V <mr.darshanv@gmail.com>
2023-02-01 10:37:34 +0530

    edit view property of topbar:

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -5,6 +5,8 @@ import { useNavigate, useParams } from "react-router"
 const Topbar = () => {
   const [search, setSearch] = useState(false)
   const navigate = useNavigate()
+  const params = useParams()
+
   return (
     <div className="flex justify-between w-full m-auto bg-white-300  sticky top-0 bg-white">
       <div
@@ -15,19 +17,34 @@ const Topbar = () => {
       >
         <TbAerialLift className="text text-6xl font text-yellow-600 hover:text-yellow-400" />
       </div>
+      {params?.id === undefined ? (
+        <div className="flex ml-auto">
+          <div className="flex flex-row">
+            <TbSearch className="m-auto text-yellow-700 text-3xl font-bold" />
+          </div>
 
-      <div className="flex ml-auto">
-        <div className="flex flex-row">
-          <TbSearch className="m-auto text-yellow-700 text-3xl font-bold" />
+          <div className="m-auto">
+            <input
+              placeholder="Search destination..."
+              className="h-10 w-96 border-2 border-orange-200 border-b-orange-500 focus:outline-none rounded-md "
+            />
+          </div>
         </div>
+      ) : null}
 
-        <div className="m-auto">
-          <input
-            placeholder="Search destination..."
-            className="h-10 w-96 border-2 border-orange-200 border-b-orange-500 focus:outline-none rounded-md "
-          />
+      {params?.id ? (
+        <div className="flex flex-row justify-evenly">
+          <div className="w-20 m-auto">
+            <span>Photos</span>
+          </div>
+          <div className="w-20 m-auto">
+            <span>Reviews</span>
+          </div>
+          <div className="w-20 m-auto">
+            <span>Location</span>
+          </div>
         </div>
-      </div>
+      ) : null}
     </div>
   )
 }
