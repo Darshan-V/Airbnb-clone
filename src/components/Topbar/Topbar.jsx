9cde9169906f07b2045a50886300ce58183c8002
Darshan-V <mr.darshanv@gmail.com>
2023-02-02 09:48:08 +0530

    add search api call

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,33 +1,52 @@
 import React, { useState } from "react"
 import { TbAerialLift, TbSearch } from "react-icons/tb"
-import { useNavigate, useParams } from "react-router"
+import { useParams } from "react-router"
+import { Link } from "react-router-dom"
+import { searchHotels } from "../lib/apiClient"
 
 const Topbar = () => {
-  const [search, setSearch] = useState(false)
-  const navigate = useNavigate()
+  const [searchedList, setSearchedList] = useState([])
   const params = useParams()
+
+  const searchProperty = async (string) => {
+    if (string.length >= 1) {
+      const searchedHotels = await searchHotels(string)
+      setSearchedList(searchedHotels)
+    }
+  }
+
+  const getSearchString = (e) => {
+    const str = e.target.value
+    searchProperty(str)
+    if (str.length === 0) {
+      setSearchedList([])
+    }
+  }
 
   return (
     <div className="flex justify-between w-full m-auto bg-white-300  sticky top-0 bg-white">
-      <div
-        className="flex justify-center mr-auto rounded-lg "
-        onClick={() => {
-          navigate("/home")
-        }}
-      >
+      <Link to={"/home"} className="flex justify-center mr-auto rounded-lg ">
         <TbAerialLift className="text text-6xl font text-yellow-600 hover:text-yellow-400" />
-      </div>
+      </Link>
       {params?.id === undefined ? (
-        <div className="flex ml-auto">
+        <div className="flex flex-col ml-auto">
           <div className="flex flex-row">
             <TbSearch className="m-auto text-yellow-700 text-3xl font-bold" />
-          </div>
 
-          <div className="m-auto">
-            <input
-              placeholder="Search destination..."
-              className="h-10 w-96 border-2 border-orange-200 border-b-orange-500 focus:outline-none rounded-md "
-            />
+            <div className="m-auto">
+              <input
+                placeholder="Search destination..."
+                className="h-10 w-96 border-2 border-orange-200 border-b-orange-500 focus:outline-none rounded-md "
+                onChange={(e) => getSearchString(e)}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col sticky top-0 bg-white">
+            {searchedList.map((hotelName, i) => (
+              <div key={i}>
+                <div>{hotelName.name}</div>
+              </div>
+            ))}
           </div>
         </div>
       ) : null}
