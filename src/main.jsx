104f21069177ec543c725d1df576d8adff55c67c
Darshan-V <mr.darshanv@gmail.com>
2023-02-03 12:18:35 +0530

    include css for leaflet library for maps

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
+import "leaflet/dist/leaflet.css"
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
