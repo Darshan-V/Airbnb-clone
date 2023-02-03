3b6310b6e1799c6616de0dc81452bb8b1137c7f9
Darshan-V <mr.darshanv@gmail.com>
2023-02-03 12:15:32 +0530

    add confirm booking page

--- src/components/Property/BookingSlots.jsx
+++ src/components/Property/BookingSlots.jsx
@@ -2,9 +2,45 @@ import React from "react"
 
 const BookingSlots = () => {
   return (
-    <div>
-      <div>
-        <p>Slot booked </p>
+    <div className="flex ">
+      <div className="flex flex-col w-96">
+        <div className="m-auto">
+          <h1 className="text-slate-800 font-sans font-semibold text-2xl">
+            Confirm Booking
+          </h1>
+        </div>
+        <div>
+          <div className="flex flex-col w-96 mr-auto border m-5">
+            <span>Your Trip</span>
+            <div>
+              <div>
+                <span>Dates</span>
+                <p>from-to month</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex m-auto w-4/5 bg-pink-600 h-10 rounded-md">
+          <button className="w-84 bg-pink-600 m-auto text-white text-semibold text-xl font-mono">
+            Continue
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-col ml-auto">
+        <div>
+          <p>Place details</p>
+        </div>
+        <div>
+          <p>Price Details</p>
+          <div>
+            <span>price x nights</span>
+            <span>Total</span>
+          </div>
+        </div>
+        <div>
+          <span>Total</span>
+          <span>Total Price</span>
+        </div>
       </div>
     </div>
   )
