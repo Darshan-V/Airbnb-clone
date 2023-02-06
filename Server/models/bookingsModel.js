015eabb98966c2e016898442b8c2854e9e397f26
Darshan-V <mr.darshanv@gmail.com>
2023-02-06 17:55:36 +0530

    fix saving data around booked dates

--- Server/models/bookingsModel.js
+++ Server/models/bookingsModel.js
@@ -5,18 +5,19 @@ const reserveSlot = async (
   checkoutDate,
   placeId,
   userId,
-  total
+  total,
+  status
 ) => {
   await pool.query(
-    `insert into bookings (check_in, check_out, property_id, user_id, total_price) values($1, $2, $3, $4, $5)`,
-    [checkinDate, checkoutDate, placeId, userId, total]
+    `insert into bookings (check_in, check_out, property_id, user_id, total_price, status) values($1, $2, $3, $4, $5 ,$6)`,
+    [checkinDate, checkoutDate, placeId, userId, total, status]
   )
   return "reserved slot"
 }
 
 const checkAvailableSlots = async (checkIn, checkOut, hotelId) => {
   const isAvailable = await pool.query(
-    "select * from bookings where check_in between $1 and $2 and check_out between $1 and $2 or check_in <= $1 and check_out >= $2 and property_id = $3",
+    "select * from bookings where $1 between check_in and check_out or $2 between check_in and check_out or $1 <= check_in and $2 >= check_out and property_id = $3",
     [checkIn, checkOut, hotelId]
   )
   return isAvailable.rows
