5d05b7a8daff95e4d462ddd04eeac91341be18e1
Darshan-V <mr.darshanv@gmail.com>
2023-02-06 18:01:15 +0530

    include react provider and chakraprovider

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,16 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import "leaflet/dist/leaflet.css"
 import "./index.css"
+import { ChakraProvider } from "@chakra-ui/react"
+import { Provider } from "react-redux"
+import { store } from "./redux/store.js"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </Provider>
   </React.StrictMode>
 )
