5235e9003acd8e36f69595194fc660bb03c9e593
Darshan-V <mr.darshanv@gmail.com>
2023-02-07 17:15:13 +0530

    add user validation

--- Server/controllers/user.js
+++ Server/controllers/user.js
@@ -4,11 +4,22 @@ import { hashPassword } from "./hashPassword.js"
 async function getUserByUserData(req, res) {
   try {
     const userId = req.params.id
+    if (!userId || userId === "undefined") {
+      res.status(403).json("require userid to access user details")
+    }
     if (Number(userId)) {
       const user = await getUserById(userId)
+      if (user.length === 0) {
+        res.status(404).json("user not found")
+        return
+      }
       res.json(user[0])
     } else {
       const user = await getUserByEmail(userId)
+      if (user.length === 0) {
+        res.status(404).json("user not found")
+        return
+      }
       res.json(user[0])
     }
   } catch (err) {
