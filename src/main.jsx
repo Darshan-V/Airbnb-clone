a0fb7dd2a5ca533536c5b2bff1a556e50752db4e
Darshan-V <mr.darshanv@gmail.com>
2023-02-17 14:18:08 +0530

    remove redux setup

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,11 @@ import App from "./App"
 import "leaflet/dist/leaflet.css"
 import "./index.css"
 import { ChakraProvider } from "@chakra-ui/react"
-import { Provider } from "react-redux"
-import { store } from "./redux/store.js"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </Provider>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 )
