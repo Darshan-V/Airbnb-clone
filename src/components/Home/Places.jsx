67a3ba5dca0fbb64ed025f2055fafb3ec43e7d1c
Darshan-V <mr.darshanv@gmail.com>
2023-02-20 14:45:04 +0530

    remove property card code and use propertycard component

--- src/components/Home/Places.jsx
+++ src/components/Home/Places.jsx
@@ -5,6 +5,7 @@ import { getHotels, searchListing } from "../lib/apiClient.js"
 import HomeTab from "./HomeTab.jsx"
 import Topbar from "../Topbar/Topbar"
 import Filter from "../pages/Filter.jsx"
+import PropertyCard from "./PropertyCard.jsx"
 
 const Places = () => {
   const [propertyData, setPropertyData] = useState([])
@@ -45,35 +46,8 @@ const Places = () => {
         <Topbar data={propertyData} change={setPropertyData} />
         <HomeTab data={propertyData} change={setPropertyData} />
         {!mapView ? (
-          <div className="grid grid-cols-1 grid-rows-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 m-auto">
-            {propertyData.map((property, i) => (
-              <div className="flex flex-row" key={i}>
-                <Link
-                  to={`/property/${property.id}`}
-                  className="flex flex-col flex-auto m-2 hover:cursor-pointer"
-                  key={i}
-                >
-                  <div className=" w-80 h-60 border rounded-md ">
-                    <img
-                      src={propertyData[i]?.imageurl[0]}
-                      className="border rounded-lg w-80 h-60"
-                    />
-                  </div>
-
-                  <div className="flex flex-col hover:cursor-pointer">
-                    <p className="font-bold text-gray-800 w-60 h-6 overflow-hidden">
-                      {property?.name}
-                    </p>
-                    <p className="font-thin text-gray-700 w-60 h-6 overflow-hidden">
-                      {property?.address?.location}
-                    </p>
-                  </div>
-                  <p className="label places-price">
-                    Rs {property?.price} night
-                  </p>
-                </Link>
-              </div>
-            ))}
+          <div className="flex w-full">
+            <PropertyCard listing={propertyData} />
           </div>
         ) : (
           <Filter data={propertyData} />
