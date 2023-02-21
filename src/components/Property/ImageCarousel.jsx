5a90603e36e17a682cff0ad155ff2b0ce5c9ba7b
Darshan-V <mr.darshanv@gmail.com>
2023-02-21 16:52:48 +0530

    include all images in the property page

--- src/components/Property/ImageCarousel.jsx
+++ src/components/Property/ImageCarousel.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const ImageCarousel = ({ images }) => {
   return (
     <div className="flex flex-row w-full h-full overflow-auto no-scrollbar">
-      {images.slice(0, 4).map((image, i) => (
+      {images.map((image, i) => (
         <img src={image} key={i} className="w-full h-96 m-1 rounded-md" />
       ))}
     </div>
