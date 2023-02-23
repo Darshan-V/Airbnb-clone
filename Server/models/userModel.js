21f1533198db2c7dca61ff996b4a601c17dc9c98
Darshan-V <mr.darshanv@gmail.com>
2023-02-23 16:16:32 +0530

    change query to send specific data from the table row

--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -1,7 +1,10 @@
 import { pool } from "./config/init.js"
 
 const getUserById = async (userId) => {
-  const user = await pool.query("select * from users where id = $1", [userId])
+  const user = await pool.query(
+    "select name,email,id from users where id = $1",
+    [userId]
+  )
   return user.rows
 }
 
