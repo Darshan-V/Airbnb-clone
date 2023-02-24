ea2df8c6f767a809e4303998d8c0021958c11d4c
Darshan-V <mr.darshanv@gmail.com>
2023-02-24 12:55:31 +0530

    add logout route

--- Server/app.js
+++ Server/app.js
@@ -6,6 +6,7 @@ import { routes as signinRouter } from "./routes/signin.js"
 import { routes as hotelRouter } from "./routes/hotel.js"
 import { routes as userRouter } from "./routes/user.js"
 import { routes as reservationRouter } from "./routes/reservation.js"
+import { routes as LogoutRouter } from "./routes/logout.js"
 
 const PORT = 8000
 
@@ -29,6 +30,8 @@ app.use("/", userRouter)
 app.use("/", reservationRouter)
 
 app.use("/", signinRouter)
+
+app.use("/", LogoutRouter)
 
 app.listen(PORT, () => {
   console.log(`Server running at port:${PORT}`)
