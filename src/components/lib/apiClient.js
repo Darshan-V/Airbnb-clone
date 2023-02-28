45b93474f2f439649d773e6ca9aa5544dee6241e
Darshan-V <mr.darshanv@gmail.com>
2023-02-28 20:50:37 +0530

    import userid with hotelslist

--- src/components/lib/apiClient.js
+++ src/components/lib/apiClient.js
@@ -9,74 +9,49 @@ async function getHotels() {
 }
 
 async function getImages(hotelId) {
-  let data = await fetch(
-    `${apiUrl}/hotels/${hotelId}/images`,
-    {
-      credentials: "include"
-    }
-  )
+  let data = await fetch(`${apiUrl}/hotels/${hotelId}/images`, {
+    credentials: "include"
+  })
   let images = await data.json()
   return images
 }
 
-async function reserveSlot(
-  hotelId,
-  checkinDate,
-  checkoutDate,
-  total,
-  status
-) {
-  let reserve = await fetch(
-    `${apiUrl}/hotels/${hotelId}/booking`,
-    {
-      method: "POST",
-      headers: {
-        "content-type": "application/json"
-      },
-      body: JSON.stringify({
-        checkIn: checkinDate,
-        checkOut: checkoutDate,
-        total: total,
-        status: status
-      }),
-      credentials: "include"
-    }
-  )
+async function reserveSlot(hotelId, checkinDate, checkoutDate, total, status) {
+  let reserve = await fetch(`${apiUrl}/hotels/${hotelId}/booking`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({
+      checkIn: checkinDate,
+      checkOut: checkoutDate,
+      total: total,
+      status: status
+    }),
+    credentials: "include"
+  })
   const data = await reserve.json()
-  console.log(data)
-
   return data
 }
 
 async function getHotelById(hotelId) {
-  let data = await fetch(
-    `${apiUrl}/hotels/${hotelId}`,
-    {
-      credentials: "include"
-    }
-  )
+  let data = await fetch(`${apiUrl}/hotels/${hotelId}`, {
+    credentials: "include"
+  })
   let hotel = await data.json()
   // console.log(hotel)
   return hotel
 }
 
 async function getUserById(userId) {
-  let data = await fetch(
-    `${apiUrl}/users/${userId}`,
-    {
-      credentials: "include"
-    }
-  )
+  let data = await fetch(`${apiUrl}/users/${userId}`, {
+    credentials: "include"
+  })
   let user = await data.json()
-  // console.log(user)
   return user
 }
 
-async function checkSlots(
-  checkIn,
-  checkOut,
-  hotelId
-) {
+async function checkSlots(checkIn, checkOut, hotelId) {
   let data = await fetch(
     `${apiUrl}/check/slots/${hotelId}/${checkIn}/${checkOut}`,
     { credentials: "include" }
@@ -86,22 +61,14 @@ async function checkSlots(
 }
 
 async function getBookingsbyProperty(hotelId) {
-  let data = await fetch(
-    `${apiUrl}/bookings/${hotelId}`,
-    {
-      credentials: "include"
-    }
-  )
+  let data = await fetch(`${apiUrl}/bookings/${hotelId}`, {
+    credentials: "include"
+  })
   const bookingData = await data.json()
   return bookingData
 }
 
-async function searchListing(
-  searchString,
-  min,
-  max,
-  hotelType
-) {
+async function searchListing(searchString, min, max, hotelType) {
   let queryUrl = `${apiUrl}/hotels/filters/categories?`
   if (searchString) {
     queryUrl += `search=${searchString}&`
