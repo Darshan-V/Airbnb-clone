e02cc5786cdeedb82361c5b099fc6f7c0a8dda65
Darshan-V <mr.darshanv@gmail.com>
2023-02-28 15:58:35 +0530

    test set cookie code by changing values of httponly and samesite

--- Server/controllers/signin.js
+++ Server/controllers/signin.js
@@ -4,18 +4,28 @@ import { authenticateUser } from "../utils/authenticateUser.js"
 async function signin(req, res) {
   try {
     const { email, password } = req.body
-    if (email.length === 0 || password.length === 0) {
+    if (
+      email.length === 0 ||
+      password.length === 0
+    ) {
       res.status(406).json("empty credentials")
     } else {
-      const [token, user] = await authenticateUser(email, password)
+      const [token, user] =
+        await authenticateUser(email, password)
       if (!token) {
         res.status(417).json("no token")
         return
       }
-      const sesssion = await createSession(user?.id, token)
+      const sesssion = await createSession(
+        user?.id,
+        token
+      )
       console.log(sesssion)
       res
-        .cookie("token", token, { httpOnly: true, sameSite: true })
+        .cookie("token", token, {
+          httpOnly: true,
+          sameSite: true
+        })
         .json("created user session ")
     }
   } catch (error) {
