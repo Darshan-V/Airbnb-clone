b56ab53f20036380ab93f6bf786f90400ca2eca6
Darshan-V <mr.darshanv@gmail.com>
2023-02-28 16:07:33 +0530

    format file

--- Server/middleware/verifyToken.js
+++ Server/middleware/verifyToken.js
@@ -2,7 +2,8 @@ import { getSession } from "../models/sessionModel.js"
 
 const verifyToken = async (req, res, next) => {
   try {
-    const reqToken = req.headers.cookie.split("=")[1]
+    const reqToken =
+      req.headers.cookie.split("=")[1]
     const session = await getSession(reqToken)
     if (session.length !== 0) {
       req.userId = session?.user_id
