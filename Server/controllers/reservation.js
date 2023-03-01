412c372163faabd7b0b99c4db1a0ee3b6e59ff95
Darshan-V <mr.darshanv@gmail.com>
2023-03-01 16:43:34 +0530

    add controller for updating booking status

--- Server/controllers/reservation.js
+++ Server/controllers/reservation.js
@@ -1,40 +1,24 @@
 import {
   checkAvailableSlots,
   reserveSlot,
-  getReservation
+  getReservation,
+  updateReservation
 } from "../models/bookingsModel.js"
 
 async function checkAvailablity(req, res) {
   try {
-    const { hotelId, checkIn, checkOut } =
-      req.params
+    const { hotelId, checkIn, checkOut } = req.params
     if (
-      checkIn <
-        new Date()
-          .toISOString()
-          .substring(0, 10) ||
+      checkIn < new Date().toISOString().substring(0, 10) ||
       checkOut <= checkIn ||
-      checkOut <=
-        new Date().toISOString().substring(0, 10)
+      checkOut <= new Date().toISOString().substring(0, 10)
     ) {
       res.status(409).json("invalid date format")
       return "invalid date format"
-    } else if (
-      !hotelId ||
-      hotelId === "undefined"
-    ) {
-      res
-        .status(406)
-        .json(
-          "connot make reservation on invalid hotel"
-        )
+    } else if (!hotelId || hotelId === "undefined") {
+      res.status(406).json("connot make reservation on invalid hotel")
     } else {
-      const bookedSlots =
-        await checkAvailableSlots(
-          checkIn,
-          checkOut,
-          hotelId
-        )
+      const bookedSlots = await checkAvailableSlots(checkIn, checkOut, hotelId)
       res.json(bookedSlots)
     }
   } catch (err) {
@@ -45,23 +29,15 @@ async function checkAvailablity(req, res) {
 async function makeBooking(req, res) {
   try {
     const hotelId = req.params.id
-    const { checkIn, checkOut, total, status } =
-      req.body
+    const { checkIn, checkOut, total, status } = req.body
     const userId = req.userId
     if (
-      checkIn <
-        new Date()
-          .toISOString()
-          .substring(0, 10) ||
+      checkIn < new Date().toISOString().substring(0, 10) ||
       checkOut <= checkIn ||
-      checkOut <=
-        new Date().toISOString().substring(0, 10)
+      checkOut <= new Date().toISOString().substring(0, 10)
     ) {
       res.status(409).json("invalid date format")
-    } else if (
-      !hotelId ||
-      hotelId === "undefined"
-    ) {
+    } else if (!hotelId || hotelId === "undefined") {
       res.status(406).json("required hotelId")
     } else if (!userId) {
       res.status(403).json("invalid user")
@@ -88,14 +64,10 @@ async function getReservedEntry(req, res) {
     const userId = req.userId
     if (!hotelId || hotelId === "undefined") {
       res.status(406).json("required hotel id")
-    } else if (
-      !userId ||
-      userId === "undefined"
-    ) {
+    } else if (!userId || userId === "undefined") {
       res.status(403).json("invalid user")
     } else {
-      const reservationData =
-        await getReservation(hotelId, userId)
+      const reservationData = await getReservation(hotelId, userId)
       res.json(reservationData)
     }
   } catch (error) {
@@ -103,8 +75,34 @@ async function getReservedEntry(req, res) {
   }
 }
 
-export {
-  checkAvailablity,
-  makeBooking,
-  getReservedEntry
+async function updateReservedSlots(req, res) {
+  const hotelId = req.params.propertyId
+  const { status, bookingId } = req.body
+  const userId = req.userId
+  if (!hotelId || hotelId === "undefined") {
+    return res.status(406).json("required hotelId")
+  }
+  if (!userId || userId === "undefined") {
+    return res.status(403).json("invalid user")
+  }
+  if (!bookingId || bookingId === "undefined") {
+    return res.status(406).json("required bookingId")
+  }
+  if (!status || !String(status) || status === undefined) {
+    return res.status(403).json("invalid entry")
+  }
+  const updateReservationData = await updateReservation(
+    propertyId,
+    userId,
+    bookingId,
+    status
+  )
+  res.json(updateReservationData)
+  try {
+  } catch (error) {
+    console.log(error.stack)
+    res.sendStatus(500)
+  }
 }
+
+export { checkAvailablity, makeBooking, getReservedEntry }
