c910563dcaa18bd8a19282d45f59eb78019207cd
Darshan-V <mr.darshanv@gmail.com>
2023-03-02 14:54:07 +0530

    add email to the req object

--- Server/middleware/verifyToken.js
+++ Server/middleware/verifyToken.js
@@ -2,11 +2,11 @@ import { getSession } from "../models/sessionModel.js"
 
 const verifyToken = async (req, res, next) => {
   try {
-    const reqToken =
-      req.headers.cookie.split("=")[1]
+    const reqToken = req.headers.cookie.split("=")[1]
     const session = await getSession(reqToken)
     if (session.length !== 0) {
       req.userId = session?.user_id
+      req.userEmail = session?.email
       req.sessionId = reqToken
       next()
     } else {
